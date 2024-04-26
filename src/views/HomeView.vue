f7ee7d9b11eb03fab8b8610a636c3626181ce47c
Alfian Luthfi <alfiankurniawan85@gmail.com>
2024-04-26 21:02:32 +0700

    dev/template - Delete Unused File

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>Halo</h1>
+  </div>
 </template>
