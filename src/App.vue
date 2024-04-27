be1f6914a9851bf8773b2ce6781c3e29a51f3315
Alfian Luthfi <alfiankurniawan85@gmail.com>
2024-04-27 18:56:12 +0700

    dev/finishing - Implement Preloader

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,83 @@
 import { RouterView } from 'vue-router'
 import Navbar from "@/components/Navbar.vue";
 import Footer from "@/components/Footer.vue";
+import Preloader from "@/components/Preloader.vue";
+</script>
+
+<script>
+export default {
+  data(){
+    return {
+      theme: 'dark',
+      loading: true,
+    }
+  },
+
+  mounted() {
+    setTimeout(() => {
+      this.loading = false;
+    }, 2000);
+
+    if (localStorage.getItem("theme") == null) {
+      localStorage.setItem("theme", "dark");
+    } else {
+      var element = document.getElementsByTagName("body")[0];
+      element.setAttribute(
+          "data-bs-theme",
+          localStorage.getItem("theme")
+      );
+      this.theme = localStorage.getItem("theme");
+    }
+  },
+
+  methods: {
+    themeToggle() {
+      var element = document.getElementsByTagName("body")[0];
+      if (element.getAttribute("data-bs-theme") === "dark") {
+        element.setAttribute("data-bs-theme", "light");
+        this.theme = "light";
+        localStorage.setItem("theme", "light");
+      } else {
+        element.setAttribute("data-bs-theme", "dark");
+        this.theme = "dark";
+        localStorage.setItem("theme", "dark");
+      }
+    },
+  },
+}
 </script>
 
 <template>
-  <div class="d-flex flex-column justify-content-between" style="height: 100vh !important">
-    <main class="flex-shrink-0">
-      <Navbar/>
-      <RouterView />
-    </main>
+  <div class="d-flex flex-column justify-content-between position-relative" style="height: 100vh !important">
+    <Transition name="fade">
+      <Preloader v-if="loading"/>
+    </Transition>
 
-    <Footer/>
+    <template v-if="!loading">
+      <main class="flex-shrink-0" >
+        <div
+            v-if="theme === 'light'"
+            @click="themeToggle"
+            title="Change to Dark Mode"
+            class="mode-toggle d-flex align-items-center justify-content-center"
+        >
+          <p class="mb-0" style="font-size: 28px">☀️</p>
+        </div>
+
+        <div
+            v-else
+            @click="themeToggle"
+            title="Change to Light Mode"
+            class="mode-toggle d-flex align-items-center justify-content-center"
+        >
+          <p class="mb-0" style="font-size: 28px">🌙</p>
+        </div>
+
+        <Navbar/>
+        <RouterView />
+      </main>
+
+      <Footer/>
+    </template>
   </div>
 </template>
---
+++ src/components/Preloader.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="preloader">
+    <div v-for="(data, index) of list" :key="'index-' + index">
+      <p v-if="index === showing" class="fs-1 text-body">
+        {{ data }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      list: ["Hello!", "¡Hola!", "Bonjour!", "Nǐ hǎo!", "Kon nichiwa!"],
+      show: 0,
+    };
+  },
+
+  computed: {
+    showing() {
+      setTimeout(() => {
+        this.show++;
+      }, 400);
+
+      return this.show;
+    },
+  },
+};
+</script>
